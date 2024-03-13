9089fde95ff35b5feb6e0b57fccac56dc6a354d9
Jefferson Rios <contacto@jeffrmdev.com>
2024-03-13 16:06:53 -0500

    Finalizado el crud

--- app_crud/urls.py
+++ app_crud/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('crear/', views.crear, name="crear")
+    path('crear/', views.crear, name="crear"),
+    path('editar_usuario/<int:persona_id>', views.guardar_edicion, name="editar_usuario"),
+    path('editar/<int:persona_id>', views.editar, name="editar"),
+    path('eliminar/<int:persona_id>', views.eliminar, name="eliminar"),
 ]
--- app_crud/views.py
+++ app_crud/views.py
@@ -23,3 +23,33 @@ def crear(request):
     )
     
     return redirect('/')
+
+
+def editar(request, persona_id):
+    persona = Persona.objects.get(id=persona_id)
+    return render(request, 'editar.html', {'persona':persona})
+
+def guardar_edicion(request, persona_id):
+    name = request.POST['name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    cedula = request.POST['cedula']
+    phone = request.POST['phone']
+    
+    persona = Persona.objects.get(id=persona_id)
+    persona.name = name
+    persona.last_name = last_name
+    persona.email = email
+    persona.cedula = cedula
+    persona.phone = phone
+    persona.save()
+    
+    return redirect('/')
+
+
+
+def eliminar(request, persona_id):
+    persona = Persona.objects.get(id=persona_id)
+    persona.delete()
+    return redirect('/')
+    
